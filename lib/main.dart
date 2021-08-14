2b698b51a2624d1abe0b698dc2fbcbd9e23d1705
craig <craig@oppkey.com>
2021-08-13 18:18:35 -0700

    adjusted ship movement for shortest angle

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
 import 'package:flame/game.dart';
 import 'package:flame/gestures.dart';
 import 'package:flutter/material.dart';
 import 'get_angle.dart';
+import 'dart:math';
 
 void main() {
   var game = MyGame();
@@ -28,9 +30,32 @@ class MyGame extends BaseGame with TapDetector {
   @override
   void onTapUp(TapUpInfo tapInfo) {
     var tapPosition = tapInfo.eventPosition.game;
-    print(tapPosition.runtimeType);
+    // print(tapPosition.runtimeType);
     var angle = getAngle(ship.position, tapPosition);
-    ship.angle = angle;
+    // move immediately to angle
+    // ship.angle = angle;
+    //
+
+    var angleDelta = ship.angle - angle;
+
+    if (angleDelta > pi) {
+      print('angleDelta is greater than pi: $angleDelta');
+
+      var shortAngle = (2 * pi - angleDelta);
+      print('proposed angle: $angle');
+
+      print('short angle $shortAngle');
+    } else if (angleDelta < -pi) {
+      print('angleDelta is less than -pi $angleDelta');
+      print('proposed angle: $angle');
+      var shorterAngle = (2 * pi - angle) * -1;
+      print('shorter angle $shorterAngle');
+      print('ship angle ${ship.angle}');
+      angle = shorterAngle;
+    }
+
+    // rotate to angle
+    ship.addEffect(RotateEffect(angle: angle, speed: 3));
     // print(degrees(angle));
   }
 }
